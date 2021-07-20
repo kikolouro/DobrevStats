13744d7bda9e964723a1f28159959493691d3df8
Francisco Louro <francisco.louro@pacsis.com>
2021-07-20 10:39:30 +0100

    Created another container to run response bot, use docker compose

--- startbot.sh
+++ startbot.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 source .env
 echo "[+] Creating docker image for bot version: $version"
-echo "[+] Running in $env envoriment"
+echo "[+] Running in $ENV envoriment"
 
-if [ "x$env" = "xdev" ] ; then
-docker build -t dobrevstats:$version . --build-arg ENV=dev ; else
-docker build -t dobrevstats:$version . ; 
-fi
-if [ "x$env" = "xdev" ] ; then
-    docker run --restart always --name dobrevstatsV$version dobrevstats:$version ; else 
-    docker run --restart always --detach --name dobrevstatsV$version dobrevstats:$version ; 
+if [ "x$ENV" = "xdev" ] ; then
+    docker-compose build --build-arg version=$version --build-arg ENV=$ENV
+    docker-compose up ;
+else 
+
+    docker-compose build --build-arg version=$version
+    docker-compose up -d
+    
 fi 
